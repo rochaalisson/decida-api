3c3ce9a6ca5a08fb20069c3ebc8844a704ebfceb
rochaalisson <alisson.rocs22@gmail.com>
2022-04-24 18:59:08 -0300

    Configuracoes para versionamento

--- src/main/java/br/com/cooperativa/decida/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/cooperativa/decida/config/swagger/SwaggerConfigurations.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
@@ -13,6 +14,7 @@ public class SwaggerConfigurations {
 	@Bean
     public OpenAPI decidaAPI() {
         return new OpenAPI()
+        		.info(new Info().version("1.0.0"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
